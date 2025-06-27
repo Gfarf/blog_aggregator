c035ec0c2aef122ecae1bce25467bb9083392bef
Gfarf <gfpalu@gmail.com>
2025-06-27 18:06:28 -0300

    safety refactor

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+
+	"github.com/Gfarf/blog_aggregator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	print(os.UserHomeDir())
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("Gustavo")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+
 }
